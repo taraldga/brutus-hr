65963094547721971bb862075352b1f17db2410c
Tarald Gåsbakk <tarald@ursolutions.no>
2020-09-23 22:05:12 +0200

    fix: cors and errors in mongoose updates

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyparser from 'body-parser';
 import mongoose from 'mongoose'
 import Employee from './Data/Employee'
+import cors from'cors';
 
 const app = express()
 const mongoConnectionString = "mongodb://develop:dev_password@127.0.0.1/brutus-hr-database"
@@ -11,16 +12,19 @@ mongoose.connect(mongoConnectionString, {useNewUrlParser: true})
 const PORT_NR = 3600
 
 app.use(bodyparser.json());
+app.use(cors())
 
 app.listen(PORT_NR, () => {
     console.log(`The server is listening on portnr ${PORT_NR}`)
 })
 
-
 app.get("/employee", async (req, res) => {
+  const limit = req.query.limit ? req.query.limit : 10;
+  const query = req.query.query ? req.query.query : {};
   try {
-    const allResults = await Employee.find();
-    res.json(allResults)
+    Employee.find(query).limit(+limit).exec((err, result) => {
+      res.json(result)
+    });
   } catch(e) {
     console.log(e)
     res.status(500).send()
@@ -28,10 +32,9 @@ app.get("/employee", async (req, res) => {
 })
 
 app.post("/employee", async (req, res) => {
-  console.log(req.body)
   try {
     const newEmployee = new Employee(req.body);
-    newEmployee.save()
+    await newEmployee.save()
     res.status(201).send()
   } catch(e) {
     res.status(500).send()
