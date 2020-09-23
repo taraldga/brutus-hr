e980244b3aaa7746c9f3a9670f13e048f64ffc0e
Tarald Gåsbakk <tarald@ursolutions.no>
2020-09-23 20:33:44 +0200

    feat: added the basic endpoints

--- backend/api.test.js
+++ backend/api.test.js
@@ -1,11 +1,18 @@
-import 'supertest'
+import request from 'supertest'
 import 'jest'
+import app from './index'
 
 
-
-
-describe("api", () => {
-  it("runs", () => {
-    expect(true).toBe(true);
-  })
-})
+describe("employee", () => {
+  it("Responds 200", async done => {
+    const response = await request(app).get("/employee");
+    expect(response.status).toBe(200);
+    done();
+  });
+  it("Responds with valid json", async done => {
+    const response = await request(app).get("/employee");
+    expect(response.status).toBe(200);
+    expect(response.header['content-type']).toMatch(/json/);
+    done();
+  });
+});
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,42 @@
 import express from 'express'
+import bodyparser from 'body-parser';
+import mongoose from 'mongoose'
+import Employee from './Data/Employee'
 
 const app = express()
+const mongoConnectionString = "mongodb://develop:dev_password@127.0.0.1/brutus-hr-database"
+mongoose.connect(mongoConnectionString, {useNewUrlParser: true})
+
 
 const PORT_NR = 3600
+
+app.use(bodyparser.json());
 
 app.listen(PORT_NR, () => {
     console.log(`The server is listening on portnr ${PORT_NR}`)
 })
 
 
-app.get("/hello", (req, res) => {
-  res.json({"Hello":  "world"})
+app.get("/employee", async (req, res) => {
+  try {
+    const allResults = await Employee.find();
+    res.json(allResults)
+  } catch(e) {
+    console.log(e)
+    res.status(500).send()
+  }
 })
 
+app.post("/employee", async (req, res) => {
+  console.log(req.body)
+  try {
+    const newEmployee = new Employee(req.body);
+    newEmployee.save()
+    res.status(201).send()
+  } catch(e) {
+    res.status(500).send()
+  }
+});
 
-// app.post("/hr")
+
+export default app
