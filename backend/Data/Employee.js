41fc11866c3b8994d76895d6a9c094238a8581d1
Tarald Gåsbakk <tarald@ursolutions.no>
2020-09-24 14:59:47 +0200

    feat: add test index

--- backend/Data/Employee.js
+++ backend/Data/Employee.js
@@ -3,16 +3,18 @@ import mongoose from 'mongoose';
 
 
 const EmployeeSchema = new mongoose.Schema({
-  first_name: String,
+  first_name: {type: String},
   last_name: String,
-  age: String,
+  age: {type: Number},
   street: String,
   city: String,
   state: String,
-  latitude: String,
-  longitude: String,
-  ccnumber: String
+  latitude: Number,
+  longitude: Number,
+  ccnumber: Number
 });
+
+EmployeeSchema.index({"age":1})
 
 const EmployeeModel = mongoose.model('Employee', EmployeeSchema)
 
